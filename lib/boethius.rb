d98658b592c78e2d939e38d991c53a48fbee417f
Gerard Keiser <gerardkeiser@gmail.com>
2017-09-12 17:34:00 -0500

    Use Rails root for dirs if gem is run inside rails

--- lib/boethius.rb
+++ lib/boethius.rb
@@ -2,11 +2,15 @@ require 'boethius/version'
 require 'boethius/source_gen'
 
 module Boethius
-  
-  # The following code is copied shamelessly from Prawn
-  file = __FILE__
-  file = File.readlink(file) if File.symlink?(file)
-  dir  = File.dirname(file)
+ 
+  if defined? ::Rails
+    dir = ::Rails.root
+  else
+    # The following code is copied shamelessly from Prawn
+    file = __FILE__
+    file = File.readlink(file) if File.symlink?(file)
+    dir  = File.dirname(file)
+  end
 
   BASEDIR = File.expand_path(File.join(dir, '..'))
   CONTEXTDIR = File.expand_path(File.join(dir, '..', 'context'))
